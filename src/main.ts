3aae6a19a156f1acf12736a31fed62f8f8f54126
Niyrme <niyrme@niyrme.xyz>
2021-04-17 23:10:40 +0200

    Update main.ts
    switched back to for of loop instead of .forEach

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ErrorMapper } from "utils/ErrorMapper";
 import { MANAGERS } from "Managers/Exports";
 
 export const loop = ErrorMapper.wrapLoop(() => {
-	MANAGERS.forEach(m => {
-		m.manage();
-	});
+	for (let manager of MANAGERS) {
+		manager.manage();
+	}
 });
