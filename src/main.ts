2ac96539269c2ffcddc31820fdfe4a67506bfc28
Niyrme <niyrme@niyrme.xyz>
2021-05-14 02:57:07 +0200

    move type declarations to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,61 @@
 import { ErrorMapper } from "utils/ErrorMapper";
 import { MANAGERS } from "Managers/Exports";
 
+declare global {
+	interface SpawnMemory {
+		minerSource?: string;
+	}
+
+	interface CreepMemory {
+		role: string;
+		isWorking?: boolean;
+
+		target?: string;
+		mode?: string;
+		sourceID?: Id<Source>;
+		room?: string;
+	}
+
+	interface RoomMemory {
+		mins: Array<{
+			name: string;
+			count: number;
+		}>;
+
+		creepMaxParts?: number;
+
+		claimRoom?: string;
+		reserveRoom?: string;
+
+		towersRepair?: boolean;
+		towersRepairWalls?: boolean;
+	}
+
+
+	interface Memory {
+		uuid: number;
+		log: any;
+		randomData: any;
+	}
+
+	interface TemplateCreep {
+		role: string;
+
+		bodyType: string;
+		body: Array<BodyPartConstant>;
+
+		creepClass: any;
+		mode?: string;
+	}
+
+	namespace NodeJS {
+		interface Global {
+			log: any;
+		}
+	}
+
+}
+
 export const loop = ErrorMapper.wrapLoop(() => {
 	for (let manager of MANAGERS) {
 		manager.manage();
